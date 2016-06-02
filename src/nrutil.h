150469a01ec6a43f4d9df851bdef9e9becb1b3f9
Pandeng Wang <wangpd35@hotmail.com>
2016-06-02 10:20:59 +0800

    Update nrutil.h

--- src/nrutil.h
+++ src/nrutil.h
@@ -79,7 +79,7 @@ void free_dmatrix(double **m, long nrl, long nrh, long ncl, long nch);
 void free_imatrix(int **m, long nrl, long nrh, long ncl, long nch);
 void free_submatrix(double **b, long nrl, long nrh, long ncl, long nch);
 void free_convert_matrix(double **b, long nrl, long nrh, long ncl, long nch);
-void free_f3tensor(float ***t, long nrl, long nrh, long ncl, long nch,\
+void free_f3tensor(double ***t, long nrl, long nrh, long ncl, long nch,\
                    long ndl, long ndh);
 void free_d3tensor(double ***t, long nrl, long nrh, long ncl, long nch,\
                    long ndl, long ndh);
